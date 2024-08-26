a45206cbde5f8bbc33bd5d369ca65b34e034894c
ioBroker Adapter Creator <noreply@iobroker.net>
2024-08-26 20:44:36 +0000

    Initial commit by adapter creator

---
+++ admin/words.js
@@ -0,0 +1,79 @@
+/* eslint no-unused-vars: off */
+/* eslint no-global-assign: off */
+/* global systemDictionary */
+"use strict";
+
+systemDictionary = {
+	"luxtronik adapter settings": {
+		"en": "Adapter settings for luxtronik",
+		"de": "Adaptereinstellungen für luxtronik",
+		"ru": "Настройки адаптера для luxtronik",
+		"pt": "Configurações do adaptador para luxtronik",
+		"nl": "Adapterinstellingen voor luxtronik",
+		"fr": "Paramètres d'adaptateur pour luxtronik",
+		"it": "Impostazioni dell'adattatore per luxtronik",
+		"es": "Ajustes del adaptador para luxtronik",
+		"pl": "Ustawienia adaptera dla luxtronik",
+		"zh-cn": "luxtronik的适配器设置"
+	},
+	"ip": {
+		"en": "Hostname / IP-Adress",
+		"de": "Hostname / IP-Adress",
+		"ru": "Hostname / IP-Adress",
+		"pt": "Hostname / IP-Adress",
+		"nl": "Hostname / IP-Adress",
+		"fr": "Hostname / IP-Adress",
+		"it": "Hostname / IP-Adress",
+		"es": "Hostname / IP-Adress",
+		"pl": "Hostname / IP-Adress",
+		"zh-cn": "Hostname / IP-Adress"
+	},
+	"ws_port": {
+		"en": "WebSocket-Port",
+		"de": "WebSocket-Port",
+		"ru": "WebSocket-Port",
+		"pt": "WebSocket-Port",
+		"nl": "WebSocket-Port",
+		"fr": "WebSocket-Port",
+		"it": "WebSocket-Port",
+		"es": "WebSocket-Port",
+		"pl": "WebSocket-Port",
+		"zh-cn": "WebSocket-Port"
+	},
+	"lt_port": {
+		"en": "Luxtronik Port",
+		"de": "Luxtronik Port",
+		"ru": "Luxtronik Port",
+		"pt": "Luxtronik Port",
+		"nl": "Luxtronik Port",
+		"fr": "Luxtronik Port",
+		"it": "Luxtronik Port",
+		"es": "Luxtronik Port",
+		"pl": "Luxtronik Port",
+		"zh-cn": "Luxtronik Port"
+	},
+	"pw": {
+		"en": "Password",
+		"de": "Password",
+		"ru": "Password",
+		"pt": "Password",
+		"nl": "Password",
+		"fr": "Password",
+		"it": "Password",
+		"es": "Password",
+		"pl": "Password",
+		"zh-cn": "Password"
+	},
+	"interval": {
+		"en": "Aktualisierungsinterval Sec.",
+		"de": "Aktualisierungsinterval Sec.",
+		"ru": "Aktualisierungsinterval Sec.",
+		"pt": "Aktualisierungsinterval Sec.",
+		"nl": "Aktualisierungsinterval Sec.",
+		"fr": "Aktualisierungsinterval Sec.",
+		"it": "Aktualisierungsinterval Sec.",
+		"es": "Aktualisierungsinterval Sec.",
+		"pl": "Aktualisierungsinterval Sec.",
+		"zh-cn": "Aktualisierungsinterval Sec."
+	}
+};
